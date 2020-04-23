f8e16231f4230a8aec1543e840d8c5108b864d4b
saraclaeys <sara.claeys01@gmail.com>
2020-04-23 16:26:56 +0200

    boll

--- js/script.js
+++ js/script.js
@@ -9,8 +9,48 @@ class Platform {
 
     draw(ctx) {
         ctx.fillStyle = Platform.color
-        ctx.fillRect = (
-            this.x, this.y, Platform.width, Platform.height,
+        ctx.fillRect(
+            this.x,
+            this.y,
+            Platform.width,
+            Platform.height,
         )
+    }
+}
+
+Platform.width = 10
+Platform.height = 100
+Platform.color = '#ff0000'
+Platform.speed = 20
+
+// Class Player extends Platform {
+
+// }
+
+class Boll {
+    constructor() {
+
+    }
+
+    setInitialProps(direction) {
+        const directionKoef = (direction === 'right') ? 1 : 1
+        this.x = fieldWidth / 2
+        this.y = fieldHeight / 2
+        this.angle = Math.random() * (Math.PI / 2) - Math.PI / 4
+        this.speed = directionKoef * Math.abs(Boll.initialSpeed)
+    }
+
+    draw(ctx) {
+        ctx.beginPath()
+        ctx.arc(
+            this.x,
+            this.y,
+            Boll.radius,
+            0,
+            2 * Math.PI,
+            false
+        )
+        ctx.fillStyle = Boll.color
+        ctx.fill()
     }
 }
